a698b57967024fc1d34b952783e39befafa1ccf8
Kimberly Rouse <ksrouse94@gmail.com>
2019-02-17 00:05:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   if array.count == 2 
     array.join(" and ")
   elsif array.count == 3
-    for (int i; i <= array.count; i++)
-    
+    for (int i; i <= array.count; i++) do
+      return "#{array[i]}, #{array[i]}, and #{array[i]}"
+    end  
   elsif array.count > 3
     array.join(", ")
     #need to insert and before last item
