cad3afe67f19ce770145e1d84417321d8ebd480b
Kimberly Rouse <ksrouse94@gmail.com>
2019-02-17 00:01:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.count == 2 
+    array.join(" and ")
+  elsif array.count == 3
+    
+  elsif array.count > 3
+    array.join(", ")
+    #need to insert and before last item
+  else
+    array.join
+  end  
 end
